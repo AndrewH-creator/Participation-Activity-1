828b9cb49caf4e66d0a79d0234bdfb0399b86f63
AndrewH-creator <hissongas@gmail.com>
2026-09-12 14:59:05 -0400

    Store sandwiches after completion

---
+++ deli.py
@@ -0,0 +1,5 @@
+# WHY: Store the sandwiches that customers ordered.
+sandwich_orders = ["turkey", "ham", "tuna", "chicken"]
+
+# WHY: Store sandwiches after they are completed.
+finished_sandwiches = []
