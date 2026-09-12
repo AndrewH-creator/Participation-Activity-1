c5c3b091fea092d458377482a8b75bdc69e63072
AndrewH-creator <hissongas@gmail.com>
2026-09-12 15:00:13 -0400

    While loop, keeps making sandwiches until completed

--- deli.py
+++ deli.py
@@ -3,3 +3,11 @@ sandwich_orders = ["turkey", "ham", "tuna", "chicken"]
 
 # WHY: Store sandwiches after they are completed.
 finished_sandwiches = []
+# WHY: Keep making sandwiches until there are no orders left.
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+
+    print(f"I made your {current_sandwich} sandwich.")
+
+    # WHY: Move the completed sandwich to the finished list.
+    finished_sandwiches.append(current_sandwich)
